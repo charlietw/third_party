b2cc42fe5864fdd2e06edc1e9054545a493a2660
HCM <HCM>
2016-12-29 11:57:37 +0000

    Up to date for lesson 6

--- project.py
+++ project.py
@@ -8,6 +8,14 @@ from flask import session as login_session
 import random
 import string
 
+# IMPORTS FOR THIS STEP
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
 app = Flask(__name__)
 
 
@@ -17,6 +25,8 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
 
 # Create anti-forgery state token
 @app.route('/login')
